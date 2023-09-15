a3290cf538130bcb1920959edfe24a367149cfcc
reuht <jhosephvillalba@gmail.com>
2023-09-15 15:44:15 -0500

    changes About me section

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../Components/Hero";
+import AboutSection from "../../Components/AboutSection";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <AboutSection /> 
     </>
   );
 };
